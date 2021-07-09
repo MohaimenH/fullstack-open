d5cbade0f7883ebad5de5be0c9d499592ad35cbf
MohaimenH <mohaimenhassan08@gmail.com>
2021-07-09 14:23:24 +0600

    Finished Part1 Codealong

--- part1/code-along/src/App.js
+++ part1/code-along/src/App.js
@@ -1,18 +1,27 @@
 import React, { useState } from "react";
 
+const Button = ({handleClick, text}) => {
+    return <button onClick={handleClick}>{text}</button>;
+};
+
+const Display = (props) => {
+    return <div>{props.value}</div>;
+};
+
+
 const App = () => {
-    const [value, setValue] = useState(10)
+    const [value, setValue] = useState(10);
 
     const setToValue = (newValue) => {
         setValue(newValue);
-    }
+    };
     
     return (
       <div>
-        {value}
-        <button onClick={() => setToValue(0)}>Reset</button>
-        <button onClick={() => setToValue(1000)}>Thousand</button>
-        <button onClick={() => setToValue(value + 1)}>Increment</button>
+        <Display value={value}/>
+        <Button handleClick={() => setToValue(0)} text="Reset"/>
+        <Button handleClick={() => setToValue(1000)} text="Thousand"/>
+        <Button handleClick={() => setToValue(value + 1)} text="Increment"/>
       </div>
     );
   };
