25ca44432e4c2085564679c1b77f97138eb9b1c4
MohaimenH <mohaimenhassan08@gmail.com>
2021-07-12 15:49:49 +0600

    Finished 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,49 +1,5 @@
 import React from "react";
-
-const Header = (props) => {
-    return <h1>{props.header}</h1>;
-};
-
-const Part = (props) => (
-    <p>
-        {props.part} {props.exercise}
-    </p>
-);
-
-const Content = ({ parts }) => {
-    return (
-        <div>
-            {parts.map((part) => (
-                <Part
-                    part={part.name}
-                    exercise={part.exercises}
-                    key={part.id}
-                />
-            ))}
-        </div>
-    );
-};
-
-const Total = ({ parts }) => {
-    let total = parts.reduce((previousValue, currentValue) => {
-        if (isNaN(previousValue)) {
-            return previousValue.exercises + currentValue.exercises;
-        }
-        return previousValue + currentValue.exercises;
-    });
-
-    return <p style={{ fontWeight: 600 }}>Number of exercises {total}</p>;
-};
-
-const Course = ({ course }) => {
-    return (
-        <div>
-            <Header header={course.name} />
-            <Content parts={course.parts} />
-            <Total parts={course.parts} />
-        </div>
-    );
-};
+import { Course } from "./Course.js";
 
 const App = () => {
     const courses = [
